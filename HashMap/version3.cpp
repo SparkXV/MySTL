fe8434532465b8acc21e9bac54af692a017de489
SparkXV <deepanshuvijay123@gmail.com>
2020-02-09 23:47:57 +0530

    using AVL

---
+++ HashMap/version3.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class AVL
+{
+     
+};
+
+class MyHashMap
+{
+    public:
+    AVL* root;
+    AVL* obj;
+};
+
+int main()
+{
+    MyHashMap hashmap;
+
+}
