72ef754cad81b88e078630122f0eeec1e01cff3f
SparkXV <deepanshuvijay123@gmail.com>
2020-03-21 13:25:33 +0530

    New commit

--- priorityqueue/heap.h
+++ priorityqueue/heap.h
@@ -1,32 +1,47 @@
 #ifndef HEAP_H
 #define HEAP_H
 
-template<typename T,string type>
+template<typename T>
 class MyPriorityQueue{
     private:
     int *array;
     int size;
     //int capacity;
+    public:
     Myheap()
     {
         array=new int[10];
         size=1;
         //capacity=1;
     }
-    void push(int data);
+    int top()
+    {
+        return array[0];
+    }
+    int getsize()  return this->size-1;
+    void pop()
+    {
+        array[0]=array[this->size-1];
+        this->size=this->size-1;
+        int n=(this->size-1)/2;
+        //for(int i=n;i>=0;i--){
+            MinHeapify(array,0,this->size-1);
+        //}   
+    }
+    void push(T data);
     void buildheap()
     {
         int n=(this->size-1)/2;
         for(int i=n;i>=0;i--){
-            heapify()
+            MinHeapify(array,0,this->size-1);
         }
     }
-    void MinHeapify();
-    void MaxHeapify();
+    void MinHeapify(int *a,int i,int n);
+   // void MaxHeapify();
 }
 
 template<typename T>
-void MyPriorityQueue<T>::MinHeapify(){
+void MyPriorityQueue<T>::MinHeapify(int *a,int i,int n){
     int left=2*i+1;
     int right=2*i+2;
     int min=i;
@@ -45,6 +60,7 @@ void MyPriorityQueue<T>::MinHeapify(){
     return;    
 }
 
+/*
 template<typename T>
 void MyPriorityQueue<T>::MaxHeapify(){
     int left=2*i+1;
@@ -64,9 +80,9 @@ void MyPriorityQueue<T>::MaxHeapify(){
     }
     return;    
 }
-
-template<typename T,string type>
-void MyPriorityQueue<T,type>::push(T data){
+*/
+template<typename T>
+void MyPriorityQueue<T>::push(T data){
     array[this->size-1]=data;
     size=size+1;
     int *temp=new int[size];
@@ -76,6 +92,6 @@ void MyPriorityQueue<T,type>::push(T data){
     }
     delete[] array;
     array=temp;
-    //heapify();
+    MinHeapify(a,0,this->size-1);
 }
 #endif
--- priorityqueue/version1.cpp
+++ priorityqueue/version1.cpp
@@ -1,9 +1,15 @@
 #include<bits/stdc++.h>
-//#include "heap.h"
+#include "heap.h"
 using namespace std;
 
 int main(){
 
-    
+    MyPriorityQueue<int>pq;
+    pq.push(9);
+    pq.push(8);
+    pq.push(7);
+    pq.push(5);
+    pq.push(1);
+    pq.push(6);
     return 0;
 }
