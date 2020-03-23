5d1101417cce3dbd0fcf709063c77b40ceaac8fa
SparkXV <deepanshuvijay123@gmail.com>
2020-03-23 22:43:21 +0530

    testing

--- priorityqueue/version1.cpp
+++ priorityqueue/version1.cpp
@@ -6,10 +6,13 @@ int main(){
 
     MyPriorityQueue<int>pq;
     pq.push(9);
+    cout<<pq.top()<<" ";
     pq.push(8);
     pq.push(7);
     pq.push(5);
     pq.push(1);
     pq.push(6);
+    
+    //pq.print();
     return 0;
 }
