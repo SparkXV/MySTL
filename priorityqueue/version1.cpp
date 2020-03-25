47ba374402bfc105e7a9c8879e81edddc7e14a2b
SparkXV <deepanshuvijay123@gmail.com>
2020-03-25 08:17:51 +0530

    Testing-Done

--- priorityqueue/version1.cpp
+++ priorityqueue/version1.cpp
@@ -1,11 +1,13 @@
 #include<bits/stdc++.h>
-#include "heap.h"
+//#include "heap.h"
+#include "maxheap.h"
 using namespace std;
 
 
 int main(){
 
     MyPriorityQueue<int>pq;
+    /*
     pq.push(9);
     pq.push(8);
     pq.push(7);
@@ -18,5 +20,19 @@ int main(){
     //cout<<pq.top()<<" ";
     
     //pq.print();
+    */
+    pq.push(1);
+    pq.push(8);
+    pq.push(7);
+    //cout<<pq.top()<<" ";
+    pq.push(2);
+    pq.push(3);
+    pq.push(9);
+    //cout<<pq.top()<<" ";
+    //cout<<pq.pop()<<" ";
+    //cout<<pq.getsize()<<" ";
+    //cout<<pq.top()<<" ";
+    
+    pq.print();
     return 0;
 }
