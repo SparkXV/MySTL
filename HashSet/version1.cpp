51e5f502b94147034a6816fa98eadcda850f9f90
SparkXV <deepanshuvijay123@gmail.com>
2020-03-26 01:42:39 +0530

    Format-update

--- HashSet/version1.cpp
+++ HashSet/version1.cpp
@@ -1,106 +1,107 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 class BST
 {
-    private:
+private:
     int key;
-    BST* left,*right;
+    BST *left, *right;
     BST(int x)
     {
-       key=x;
-       left=NULL;
-       right=NULL;
-    } 
-    void insert(BST* root,int key)
+        key = x;
+        left = NULL;
+        right = NULL;
+    }
+    void insert(BST *root, int key)
     {
-        if(key>=root->key)
+        if (key >= root->key)
         {
-            if(root->right==NULL)
+            if (root->right == NULL)
             {
-                BST* temp=new BST(key);
-                root->right=temp;
+                BST *temp = new BST(key);
+                root->right = temp;
             }
             else
-               insert(root->right,key);
+                insert(root->right, key);
         }
         else
         {
-            if(root->left==NULL)
+            if (root->left == NULL)
             {
-                BST* temp=new BST(key);
-                root->left=temp;
+                BST *temp = new BST(key);
+                root->left = temp;
             }
             else
-                insert(root->left,key);
+                insert(root->left, key);
         }
     }
-    bool search(BST* temp,int key)
+    bool search(BST *temp, int key)
     {
-        if(temp==NULL)
-          return false;
-        if(temp->key==key)
+        if (temp == NULL)
+            return false;
+        if (temp->key == key)
         {
             return true;
         }
-        else if(temp->key>key)
+        else if (temp->key > key)
         {
-            return search(temp->left,key);
+            return search(temp->left, key);
         }
         else
         {
-           return search(temp->right,key);
-        }  
+            return search(temp->right, key);
+        }
     }
     friend class MyHashset;
 };
 class MyHashset
 {
-    public:
-    BST* root;
-    BST* obj;
+public:
+    BST *root;
+    BST *obj;
     MyHashset()
     {
-        root=NULL;
+        root = NULL;
     }
     void put(int key)
     {
-        if(root==NULL)
+        if (root == NULL)
         {
-            BST* temp=new BST(key);
-            root=temp;
+            BST *temp = new BST(key);
+            root = temp;
         }
         else
         {
-            bool t=obj->search(root,key);
-            if(t)
+            bool t = obj->search(root, key);
+            if (t)
             {
                 return;
             }
             else
             {
-                obj->insert(root,key);
-            }   
+                obj->insert(root, key);
+            }
         }
     }
     bool find(int key)
     {
-        return obj->search(root,key);
+        return obj->search(root, key);
     }
-    
 };
-int main() 
-{ 
+int main()
+{
     MyHashset hashset;
-	hashset.put(1);
-	hashset.put(2);
-    if(hashset.find(6))
+    hashset.put(1);
+    hashset.put(2);
+    if (hashset.find(6))
     {
-        cout<<"true"<<"\n";
+        cout << "true"
+             << "\n";
     }
     else
     {
-        cout<<"false"<<"\n";
+        cout << "false"
+             << "\n";
     }
-    return 0; 
-} 
+    return 0;
+}
