ea82e050b399ed1263fbc2683be0ee1bf55e6099
SparkXV <deepanshuvijay123@gmail.com>
2020-03-29 19:07:57 +0530

    Header file -Deque

--- deque/deque.h
+++ deque/deque.h
@@ -1,7 +1,16 @@
 #ifndef DEQUE_H
 #define DEQUE_H
 
+template<typename T>
 class Mydeque{
-
+    public:
+        void push_front(int data);
+        void push_back(int data);
+        int pop_front();
+        int pop_back();
+        int getsize();
+        bool empty();
+        int front();
+        int back();
 };
 #endif
