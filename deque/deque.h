8a1a8acd44c50fbf6c37bc26100a9f35ab78bc6e
SparkXV <deepanshuvijay123@gmail.com>
2020-03-31 13:35:45 +0530

    Header File

--- deque/deque.h
+++ deque/deque.h
@@ -1,16 +1,32 @@
 #ifndef DEQUE_H
 #define DEQUE_H
 
+#define MAX 1000
+
 template<typename T>
 class Mydeque{
     public:
-        void push_front(int data);
-        void push_back(int data);
-        int pop_front();
-        int pop_back();
+        int front;
+        int rear;
+        T array[MAX];
+        int size;
+        Mydeque(int size);
+        void push_front(T data);
+        void push_back(T data);
+        void pop_front();
+        void pop_back();
         int getsize();
         bool empty();
         int front();
         int back();
 };
+
+template<typename T>
+Mydeque<T>::Mydeque(int size){
+
+}
+template<typename T>
+void Mydeque<T>::push_front(T data){
+
+}
 #endif
