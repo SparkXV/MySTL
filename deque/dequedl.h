4222e6c2e6e7d0231fb46e8e4f4552f5eaeb2622
SparkXV <deepanshuvijay123@gmail.com>
2020-04-01 21:39:06 +0530

    Header File-dll

---
+++ deque/dequedl.h
@@ -0,0 +1,9 @@
+#ifndef DEQUEDL_H
+#define DEQUEDL_H
+
+                    /*   
+                        Implementation using Doubly Singly Linked List    
+                                                                        */
+
+
+#endif
