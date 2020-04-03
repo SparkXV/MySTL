701ad63f429df844c0ea925e31fc60347cdca3d8
SparkXV <deepanshuvijay123@gmail.com>
2020-04-03 16:41:03 +0530

    Header File

--- deque/dequedl.h
+++ deque/dequedl.h
@@ -5,5 +5,19 @@
                         Implementation using Doubly Singly Linked List    
                                                                         */
 
+template<typename T>
+class Node
+{
+    T data;
+    Node* next;
+    Node* prev;
+    Node();
+};
 
+template<typename T>
+Node<T>::Node(T data){
+    this->data=data;
+    this->next=NULL;
+    this->prev=NULL;
+}
 #endif
