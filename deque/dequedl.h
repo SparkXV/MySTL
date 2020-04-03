caf5e393501c11b1163e3a3a6b5cc2754a4fffd2
SparkXV <deepanshuvijay123@gmail.com>
2020-04-03 18:01:56 +0530

    Update

--- deque/dequedl.h
+++ deque/dequedl.h
@@ -1,29 +1,30 @@
 #ifndef DEQUEDL_H
 #define DEQUEDL_H
+//#include "Node.h"
+
 
                     /*   
-                        Implementation using Doubly Singly Linked List    
-                                                                        */
+                        Implementation using Doubly Singly Linked List    */
 
 template<typename T>
 class Node
 {
-    public:
-    T data;
+    private:
+    int data;
     Node* next;
     Node* prev;
-    Node(T data);
-};
+    Node(T data)
+    {
+        this->data=data;
+        this->next=NULL;
+        this->prev=NULL;
+    }
+    friend class Mydeque; 
+};    
 
 template<typename T>
-Node<T>::Node(T data){
-    this->data=data;
-    this->next=NULL;
-    this->prev=NULL;
-}
-
-template<typename T>
-class Mydeque{
+class Mydeque
+{
     public:
     Node *front;
     Node *rear;
@@ -34,13 +35,15 @@ class Mydeque{
     void pop_front();
     void pop_back();
     bool empty();
+    int getsize();
     int getfront();
-    int getback();
-    bool isfull();        
-}
+    int getback();      
+};
+
 template<typename T>
-Mydeque<T>::Mydeque(){
-    this->front=NULL:
+Mydeque<T>::Mydeque()
+{
+    this->front=NULL;
     this->rear=NULL;
     this->size=0;
 }
@@ -49,10 +52,11 @@ void Mydeque<T>::push_front(T data){
 
     Node* temp=new Node(data);
     if(temp==NULL){
-        cout<<"Memory Error"<<"\n";
+        std::cout<<"Memory Error"<<"\n";
         return;
     }
-    else{
+    else
+    {
         if(this->front==NULL)
         {
             this->rear=this->front=temp;
@@ -71,10 +75,11 @@ template<typename T>
 void Mydeque<T>::push_back(T data){
     Node* temp=new Node(data);
     if(temp==NULL){
-        cout<<"Memory Error"<<"\n";
+        std::cout<<"Memory Error"<<"\n";
         return;
     }
-    else{
+    else
+    {
         if(this->rear==NULL)
         {
             this->rear=this->front=temp;
@@ -92,7 +97,82 @@ void Mydeque<T>::push_back(T data){
 template<typename T>
 void Mydeque<T>::pop_front(){
 
-    if(front==NULL)
-
+    if(empty()){
+        std::cout<<"Segementation Fault"<<"\n";
+        return;
+    }
+    else
+    {
+        Node *temp=this->front;
+        this->front=this->front->next;
+        if(this->front==NULL)
+        {
+            rear=NULL;
+        }
+        else
+        {
+            this->front->prev=NULL;
+        }
+        free(temp);
+        size--;
+    }
 }
+
+template<typename T>
+void Mydeque<T>::pop_back()
+{
+    if(empty())
+    {
+        std::cout<<"Segementation Fault"<<"\n";
+        return;
+    }
+    else
+    {
+        Node *temp=this->rear;
+        this->rear=this->rear->prev;
+        if(this->rear==NULL)
+        {
+            front=NULL;
+        }
+        else
+        {
+            this->rear->next=NULL;
+        }
+        free(temp);
+        size--;
+    }
+}
+
+template<typename T>
+bool Mydeque<T>::empty(){ 
+    return front==NULL;
+}
+
+template<typename T>
+int Mydeque<T>::getsize(){
+   return size;
+}
+
+template<typename T>
+int Mydeque<T>::getfront(){
+    if (empty()) 
+    { 
+        std::cout << "Underflow" << "\n";
+        return -1 ; 
+    } 
+    return this->front->data;
+}
+
+template<typename T>
+int Mydeque<T>::getback(){
+    if(empty()) 
+    { 
+        std::cout << " Underflow" << "\n";
+        return -1 ; 
+    } 
+    return this->rear->data;
+}
+
+
+
 #endif
