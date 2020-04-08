26c1f4ff817b88fea8af3e230168210af9866f41
SparkXV <deepanshuvijay123@gmail.com>
2020-04-08 16:51:44 +0530

    Done on deque file

--- deque/testing.cpp
+++ deque/testing.cpp
@@ -1,10 +1,16 @@
 #include<bits/stdc++.h>
-#include "dequedl.h"
+#include "deque.h"
 using namespace std;
 
 int main(){
 
-    Mydeque<int>dq;
-    cout<<dq.getsize()<<"\n";
+    Mydeque<int>dq(5);
+    dq.push_front(5);
+    cout<<dq.getfront()<<" ";
+    dq.push_back(7);
+    cout<<dq.getback()<<" ";
+    dq.pop_back();
+    dq.pop_front();
+    cout<<dq.getback()<<" ";
     return 0;
 }
